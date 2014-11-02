02f0be34c3e73075385d3b6cdc95b9b07ffa6e98
cavasanchez <c.vazquezsa@alumnos.urjc.es>
2014-11-02 22:16:34 +0100

    Agregado Tail a medias

--- probandoTail.c
+++ probandoTail.c
@@ -4,7 +4,8 @@
 #define TAM_MAX 1024
 
 int main(int argc, char *argv[]) {
-	int i = 0;	
+	int i = 0;
+	int j=0;
 	int numeroElementos;
 	numeroElementos=atoi(argv[1]);
 	if(numeroElementos==0) {
@@ -16,7 +17,7 @@ int main(int argc, char *argv[]) {
 
 	char** guarda;
 //guarda=inicializarArrayPunteros(numeroElementos);
-	
+
 	guarda= (char**) malloc (numeroElementos*sizeof(char*));
 	for(i=0;i<numeroElementos;i++){
 	guarda[i]=(char*) malloc (TAM_MAX*sizeof(char));
@@ -25,25 +26,27 @@ int main(int argc, char *argv[]) {
 	//cadenaEntrada será donde se almacene la entrada
 	char* cadenaEntrada;
 	cadenaEntrada = (char *) malloc (TAM_MAX*sizeof(char));
-	
-	
-	for (i=0; i <= numeroElementos-1; i++) {
-		fgets(cadenaEntrada, TAM_MAX, stdin);
-		strcpy(guarda[i],cadenaEntrada);			
-	}
-	printf("\n");
+
+	while(fgets(cadenaEntrada, TAM_MAX, stdin) != NULL){
+		printf("ENTRA WHILE");
+		int aux=(j%numeroElementos);
+		printf("El numero es :%i \n",aux);
+		j++;
+		strcpy(guarda[(j%numeroElementos)],cadenaEntrada);
+}
+	printf("FICHERO CERRADO \n");
 	printf("*****EL RESULTADO ES:*****\n");
 	imprimirArray(guarda,numeroElementos-1);
+	printf("\n");
 	free(guarda);
 	free(cadenaEntrada);
 	return 0;
-	
 };
 
 void imprimirArray(char** array, int tamanio){
 int i=0;
 	for(i=0;i<=tamanio;i++){
-		printf("%s", array[i]);	
+		printf("%s", array[i]);
 	}
 };
 char** inicializarArrayPunteros(int numero){
