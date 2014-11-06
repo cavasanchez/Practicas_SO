42cfccd4c8ef0f23faf53e33d661e7c6c682b7fb
cavasanchez <c.vazquezsa@alumnos.urjc.es>
2014-11-06 11:43:05 +0100

    Añadido al longLines método que conprueba si el array está lleno

--- probandoLonglines.c
+++ probandoLonglines.c
@@ -14,7 +14,6 @@ int main (int argc, char *argv[]) {
 	int tam_menor;	// tam_menor: longitud del string menor del array
 
 	// 1) En caso de que no se haya pasado un parametro o este sea cero, default: head(10)
-
 	if (argv[1] == NULL || argv[1] == 0) {
 		printf("El parametro introducido no es correcto. Por defecto se ejecutara head(10)\n");
 		n=10;
@@ -23,39 +22,38 @@ int main (int argc, char *argv[]) {
 	}
 
 	// 2) solucion: array de punteros que actuara como buffer.
-
-	char** solucion;	
+	char** solucion;
 
 	// 3) Inicialiamos solucion reservando espacio suficiente.
-
 	solucion= (char**) malloc (n*sizeof(char*));
 	for (i=0;i<n;i++) {
 		solucion[i]=(char*) malloc (TAM_MAX*sizeof(char));
 	}
 
 	// 4) entrada: string donde se almacena la entrada
-
 	char* entrada;
 	entrada = (char *) malloc (TAM_MAX*sizeof(char));
 
 	// 5) Leemos entrada y almacenamos en entrada
-
 	i=0;
 	tam_menor=0;
 	while(fgets(entrada, TAM_MAX, stdin) != NULL){
 		if (strlen(entrada)>tam_menor) {
-			int nueva_posicion = menor (solucion,strlen(entrada));
-			insertar(solucion,entrada,nueva_posicion);
+				if(esta_lleno(solucion,n)==1){
+					printf("ARRAY NO LLENO \n");
+
+				}
+				else{
+					printf("ARRAY YA LLENO \n");
+					int nueva_posicion = menor (solucion,strlen(entrada));
+					insertar(solucion,entrada,nueva_posicion);
+				}
 		}
-		i++;
 	}
-
 	// 6) Imprimimos la solucion
-
 	imprimir(solucion);
 
 	// 7) Liberamos el espacio usado
-
 	for (i=0;i<n;i++) {
 		free(solucion[i]);
 	}
@@ -65,7 +63,6 @@ int main (int argc, char *argv[]) {
 }
 
 // imprimir: metodo que imprime la solucion por pantalla
-
 void imprimir (char** string) {
 	printf("\n ##### La Solucion es: ##### \n");
 	int i;
@@ -75,7 +72,6 @@ void imprimir (char** string) {
 }
 
 // longitud: metodo que devuelve la longitud de una lista de punteros
-
 int longitud (char** string) {
 	int i=0;
 	while (string[i] != NULL) {
@@ -85,8 +81,8 @@ int longitud (char** string) {
 }
 
 // menor: metodo que devuelve la posicion donde insertar el nuevo
-
 int menor (char** solucion, int nuevo){
+	printf("entra a menor \n");
 	int i;
 	for (i=0;i<longitud(solucion);i++) {
 		if (nuevo<strlen(solucion[i])) {break;}
@@ -95,8 +91,8 @@ int menor (char** solucion, int nuevo){
 }
 
 // insertar: metodo que inserta el nuevo valor en el array
-
 void insertar (char** solucion, char* entrada, int nueva_posicion) {
+	printf("Intenta añadir en la posicion %i \n",nueva_posicion);
 	int i;
 	//if (esta_lleno) {
 	for (i=longitud(solucion);i>nueva_posicion;i--) {
@@ -105,4 +101,17 @@ void insertar (char** solucion, char* entrada, int nueva_posicion) {
 	strcpy(solucion[nueva_posicion],entrada);
 }
 
-// se le va la bola con los impares (otra vez)
+int esta_lleno(char** solucion,int tamanio_array){
+int lleno=0;
+int i;
+int aux=0;
+for(i=0;i<longitud(solucion);i++){
+	if(solucion[i]!=NULL){
+		aux++;
+	}
+}
+if(aux==tamanio_array){
+	lleno=1;
+}
+return lleno;
+}
--- probandoTail.c
+++ probandoTail.c
@@ -11,7 +11,6 @@ int main (int argc, char *argv[]) {
 	int n;	// n: numero de strings a guardar
 
 	// 1) En caso de que no se haya pasado un parametro o este sea cero, default: head(10)
-
 	if (argv[1] == NULL || argv[1] == 0) {
 		printf("El parametro introducido no es correcto. Por defecto se ejecutara head(10)\n");
 		n=10;
@@ -20,23 +19,19 @@ int main (int argc, char *argv[]) {
 	}
 
 	// 2) solucion: array de punteros que actuara como buffer.
-
-	char** solucion;	
+	char** solucion;
 
 	// 3) Inicialiamos solucion reservando espacio suficiente.
-
 	solucion= (char**) malloc (n*sizeof(char*));
 	for (i=0;i<n;i++) {
 		solucion[i]=(char*) malloc (TAM_MAX*sizeof(char));
 	}
 
 	// 4) entrada: string donde se almacena la entrada
-
 	char* entrada;
 	entrada = (char *) malloc (TAM_MAX*sizeof(char));
 
 	// 5) Leemos entrada y almacenamos en entrada
-
 	i=0;
 	while (fgets(entrada,TAM_MAX,stdin) != NULL) {
 		j=i%n;
@@ -46,22 +41,18 @@ int main (int argc, char *argv[]) {
 	}
 
 	// 6) Imprimimos la solucion
-
 	imprimir(solucion,j+1,n);
 
 	// 7) Liberamos el espacio usado
-
 	i=0;
 	for (i=0;i<n;i++) {
 		free(solucion[i]);
 	}
 	free(entrada);
-
 	return 0;
 }
 
 // imprimir: metodo que imprime la solucion por pantalla
-
 void imprimir (char** string, int j, int tam) {
 	printf("\n ##### La Solucion es: ##### \n");
 	int i=0;
@@ -70,5 +61,3 @@ void imprimir (char** string, int j, int tam) {
 		printf("%s",string[aux]);
 	}
 }
-
-// se le va la bola con los impares
