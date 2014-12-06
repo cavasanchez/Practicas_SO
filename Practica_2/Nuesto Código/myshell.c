2b54d58808c9392307cb2142f8df4ea6e6410793
cavasanchez <cavasanchez@gmail.com>
2014-12-06 13:51:00 +0100

    Solo un mandato funciona

--- Practica_2/Nuesto Código/myshell.c
+++ Practica_2/Nuesto Código/myshell.c
@@ -5,25 +5,46 @@
 #include "parser.h"
 #define TAM 1024
 
+void senal (int s){
+  if(s!=0){                                       //ACTIVAR
+    signal(SIGINT,SIG_DFL);
+    signal(SIGQUIT,SIG_DFL);
+  }
+  else{                                           //DESACTIVAR
+    signal(SIGINT,SIG_IGN);
+    signal(SIGQUIT,SIG_IGN);
+  }
+}
+
 int main(void){
   tline* linea;
   char* entrada;
   pid_t pid;
-  int i;
+  int i=0;
   entrada=(char*) malloc(TAM*sizeof(char));
-  for(;;){
-    printf("$ ");
-    fgets(entrada,TAM,stdin);
+
+  //DESACTIVAMOS SEÑALES
+  senal(0);
+  printf("$ ");
+
+  while(fgets(entrada,TAM,stdin)){
+    printf("SE HAN METIDO %i MANDATOS \n",linea->ncommands);
     linea=tokenize(entrada);
-    for(i=0;i<linea->ncommands-1;i++){
+    for(i=0; i<linea->ncommands; i++){
+      printf("ENTRA FOR\n");
       pid = fork();
-      printf("*****");                                         //Creando hijos
-      if (linea->commands[0].filename==NULL){
-        printf("mandato: No se encuentra el mandato \n");
-      }
-      else{
-        //system(linea->commands[0].argv[0]);
+      printf("CREA UN HIJO\n");
+      if(pid==0){
+        printf("SOY EL HIJO\n");
+        senal(1);
+        pid=execvp(linea->commands[0].argv[0],linea->commands[0].argv);
+        if (pid==-1){
+          printf("mandato: No se encuentra el mandato \n");
+          return 0;
+        }
       }
     }
+    printf("SALE DEL FOR\n");
   }
+  return 0;
 }
